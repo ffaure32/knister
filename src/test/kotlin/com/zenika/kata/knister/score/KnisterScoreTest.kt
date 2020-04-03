1effb86bdeb9c9af70a73edaa4d4c648f001ede3
ffaure32 <ffaure32@yahoo.fr>
2020-04-03 11:27:59 +0200

    preparation tests scoring

---
+++ src/test/kotlin/com/zenika/kata/knister/score/KnisterScoreTest.kt
@@ -0,0 +1,12 @@
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.*
+
+class KnisterScoreTest {
+    @Nested
+    inner class LineScoreTest {
+        @Test
+        fun `pair scores 1 point`() {
+            assertThat(true).isEqualTo(true)
+        }
+    }
+}
