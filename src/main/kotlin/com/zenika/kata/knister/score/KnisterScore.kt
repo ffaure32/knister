73b3cd71b92518bbb97cb947b3e441f2628fd6f2
ffaure32 <ffaure32@yahoo.fr>
2020-04-03 17:56:36 +0200

    version avec enum pour figures

--- src/main/kotlin/com/zenika/kata/knister/score/KnisterScore.kt
+++ src/main/kotlin/com/zenika/kata/knister/score/KnisterScore.kt
@@ -1,15 +1,44 @@
-class Line(val diceRolls : IntArray) {
-    fun score() : Int {
-        return when (diceRolls.toSet().size) {
-            4 -> 1
-            3 -> 3
-            2 -> fullHouseOrFourOfAKind()
-            1 -> 10
-            else -> 0
-        }
+enum class Figure(val combination: IntArray, val score: Int) {
+    PAIR(intArrayOf(2, 1, 1, 1), 1),
+    DOUBLE_PAIR(intArrayOf(2, 2, 1), 3),
+    THREE_OF_A_KIND(intArrayOf(3, 1, 1), 3),
+    FOUR_OF_A_KIND(intArrayOf(4, 1), 6),
+    FULL_HOUSE(intArrayOf(3, 2), 8),
+    YAMS(intArrayOf(5), 10)
+
+}
+
+fun findFigure(dices: IntArray): Figure? {
+    return Figure.values().find { it.combination contentEquals dices }
+}
+
+class Line(private val diceResults: IntArray) {
+    private val COMMON_STRAIGHT_DICE_ROLL = 7
+
+    fun score(): Int {
+        val figureCombination = getDiceRollsFigure()
+        val figure = findFigure(figureCombination)
+        return figure?.score ?: nonFigureScore()
     }
 
-    fun fullHouseOrFourOfAKind(): Int {
-        return if (diceRolls.groupBy{it}.mapValues{it.value.size}.values.any{it == 1}) 6 else 8
+    private fun getDiceRollsFigure(): IntArray {
+        val scoresByValue = diceResults.groupBy { it }.mapValues { it.value.size }
+        return scoresByValue.values.sortedDescending().toIntArray()
     }
+
+
+    private fun nonFigureScore(): Int {
+        return if (isStraight())
+            straightScore()
+        else 0
+    }
+
+    private fun straightScore(): Int {
+        return if (diceResults.contains(COMMON_STRAIGHT_DICE_ROLL))
+            8
+        else 12
+    }
+
+    private fun isStraight() = (diceResults.max()!! - diceResults.min()!! == 4)
+
 }
